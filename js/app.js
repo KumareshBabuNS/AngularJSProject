9ffe71bc4c17bd00032680a7b36d98ac9d11f502
Richa Bhatia <bhatia.ri@husky.neu.edu>
2017-03-11 21:17:09 -0500

    Added new controller ReviewController to add reviews to the reviews array and added ng-submit

--- js/app.js
+++ js/app.js
@@ -25,6 +25,15 @@
 		};
 	});
 
+	app.controller('ReviewController', function(){
+		this.review = {};
+
+		this.addReview = function(product){
+			product.reviews.push(this.review);
+
+		};
+	});
+
 
 	var gems = [{
 
